5ec9ce11cd8dd2c920e43a7c398af6491ac896d9
jyootai <jyootg@gmail.com>
2015-12-24 17:19:06 +0800

    webpack config setting

---
+++ front/webpack.config.js
@@ -0,0 +1,11 @@
+/**
+ * @file webpack.config.js
+ * @author jyootai
+ * @description webpack 配置文件
+ */
+module.exports = {
+  entry: './app/main.js',
+  output: {
+  filename: 'bundle.js'       
+  }
+};
